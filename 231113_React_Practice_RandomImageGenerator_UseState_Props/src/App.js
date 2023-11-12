17c20f234e29145d6def3c6f1e1562f883d4d49b
Emrullah Yabasun <yabasunemrullah@gmail.com>
2023-11-13 01:36:57 +0300

    Random Image Generator
    A react practice using props and states for generating a random images each time when you press the button

---
+++ 231113_React_Practice_RandomImageGenerator_UseState_Props/src/App.js
@@ -0,0 +1,44 @@
+import Course from './Course'
+import './App.css'
+import { useState } from 'react'
+
+
+
+
+function getRandomCourse() {
+  const courseArray = ['Angular', 'Bootstrap5', 'Ccsharp', 'Kompleweb'];
+  return courseArray[Math.floor(Math.random() * courseArray.length)]
+}
+
+
+
+
+
+function App() {
+
+  const [courses, setCourses] = useState([]);
+
+  const handleClick = () => {
+    setCourses([...courses, getRandomCourse()])
+  }
+
+
+  const courseList = courses.map((item, index) => {
+    return (
+      <Course
+        key={index}
+        courseName={item} /> //courseName isimli bir prop yapmis olduk ve mapteki itemini atadik
+    )
+  });
+
+
+  return (
+    <div className="App">
+      <button onClick={handleClick}>Kurs Ekle</button>
+      {courseList}
+
+    </div>
+  );
+}
+
+export default App;
