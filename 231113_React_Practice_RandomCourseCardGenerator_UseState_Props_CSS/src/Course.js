fd0eb6d020fa7b62b5ec58d233ed7062669e4689
Emrullah Yabasun <yabasunemrullah@gmail.com>
2023-11-13 23:03:31 +0300

    CourseCardGenerator
    ReactPractice_RandomCourseCardGenerator_UseState_Props_CSS

---
+++ 231113_React_Practice_RandomCourseCardGenerator_UseState_Props_CSS/src/Course.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Angular from './assets/angular.jpg'
+import Bootstrap5 from './assets/bootstrap5.png'
+import Ccsharp from './assets/ccsharp.png'
+import Kompleweb from './assets/kompleweb.jpg'
+import './Course.css'
+
+const courseMap = {
+  Angular: Angular,
+  Bootstrap5: Bootstrap5,
+  Ccsharp: Ccsharp,
+  Kompleweb: Kompleweb,
+}  //bir mapping objesi olusturuldu.
+
+function Course({ courseName }) { //courseName propu bu componente tanimlandi
+  return (
+    <div className='courseDiv'>
+      <img className='course' src={courseMap[courseName]} alt="" />
+    </div>
+  )
+}
+
+export default Course
